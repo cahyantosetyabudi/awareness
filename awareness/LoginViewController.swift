708d6a6e64e438d0ffc1a8173530016908aeb3c6
Cahyanto Setya Budi <executionzee@gmail.com>
2018-11-06 13:55:40 +0700

    add login page

---
+++ awareness/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  awareness
+//
+//  Created by Cahyanto Setya Budi on 11/6/18.
+//  Copyright © 2018 Cahyanto Setya Budi. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
