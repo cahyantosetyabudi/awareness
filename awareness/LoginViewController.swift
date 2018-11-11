41cbf1986987a9e00b59a9817d7c6e3830cd175d
Cahyanto Setya Budi <executionzee@gmail.com>
2018-11-11 13:22:32 +0700

    add login feature

--- awareness/LoginViewController.swift
+++ awareness/LoginViewController.swift
@@ -13,9 +13,15 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
+    var login_token: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let preferences = UserDefaults.standard
+        if preferences.object(forKey: "token") != nil {
+            login_token = preferences.object(forKey: "token") as! String
+        }
     }
     
     @IBAction func emailTextField(_ sender: UITextField) {
@@ -39,43 +45,61 @@ class LoginViewController: UIViewController {
         let email = emailField.text
         let password = passwordField.text
         if email!.isEmpty || password!.isEmpty {
-            print("Email or password is empty")
+            self.displayMesage("Email or Password is required")
         }
+        
         let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
         activityIndicator.center = view.center
         activityIndicator.hidesWhenStopped = false
         activityIndicator.startAnimating()
         view.addSubview(activityIndicator)
-        
+
         //Request http for authentication
         let url = URL(string: "http://128.199.83.121:5021/sign-in")
         let httpBody = ["email": email!, "password": password!] as [String: String]
         let session = URLSession.shared
         var request = URLRequest(url: url!)
-        
+
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         do {
             request.httpBody = try JSONSerialization.data(withJSONObject: httpBody, options: .prettyPrinted)
         } catch let error {
             print(error.localizedDescription)
+            self.displayMesage("Something gone wrong with server")
         }
-        
+
         let task = session.dataTask(with: request) { (data, response, error) in
-            if error != nil {
-                print("Error in request \(error?.localizedDescription)")
-            }
+            self.removeActivityIndicator(activityIndicator)
             
+            guard let data = data else {
+                return
+            }
+
             do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
-                print(json)
+                let jsonObject = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+                guard let json = jsonObject as? NSDictionary else {
+                    return
+                }
+                if let token = json["token"] as? String {
+                    self.login_token = token
+                    let preferences = UserDefaults.standard
+                    preferences.set(self.login_token, forKey: "token")
+                    
+                    DispatchQueue.main.async {
+                        self.segueToTabBar()
+                    }
+                } else {
+                    self.displayMesage("Email or Password Wrong")
+                }
+
             } catch {
-                print("Error when seriliazation json")
+                print("Error: \(error.localizedDescription)")
+                self.removeActivityIndicator(activityIndicator)
             }
-            
+
         }
         task.resume()
-        
     }
     
 }
@@ -89,5 +113,26 @@ extension LoginViewController {
         UIView.setAnimationDuration(movementDuration)
         self.view.frame = self.view.frame.offsetBy(dx: 0, dy: movement)
         UIView.commitAnimations()
+    }
+    
+    func segueToTabBar() {
+        let mainTabBarController = self.storyboard?.instantiateViewController(withIdentifier: "MainTabBarController") as! MainTabBarController
+        self.present(mainTabBarController, animated: true, completion: nil)
+    }
+    
+    func removeActivityIndicator(_ activityIndicator: UIActivityIndicatorView) {
+        DispatchQueue.main.async {
+            activityIndicator.stopAnimating()
+            activityIndicator.removeFromSuperview()
+        }
+    }
+    
+    func displayMesage(_ messages: String) {
+        DispatchQueue.main.async {
+            let alertController = UIAlertController(title: "Alert", message: messages, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
     }
 }
---
+++ awareness/MainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarController.swift
+//  awareness
+//
+//  Created by Adam Mukharil Bachtiar on 11/11/18.
+//  Copyright © 2018 Cahyanto Setya Budi. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        selectedIndex = 2
+    }
+    
+}
